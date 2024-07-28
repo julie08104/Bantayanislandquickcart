2ed5a5c239573430b6cb44c3a7507040edfbf539
Julie Ann Almohallas <almohallasjulieann08@gmail.com>
2024-07-28 19:46:32 +0800

    commit

--- app/config/config.php
+++ app/config/config.php
@@ -9,13 +9,13 @@ define('SITE_ROOT', 'http://localhost/ample/');
 
 // Database Information
 // Database Hostname
-define('DATABASE_HOST','localhost');
+define('DATABASE_HOST','127.0.0.1');
 // Database Username
-define('DATABASE_USER','root');
+define('DATABASE_USER','u510162695_ample');
 // Database Name
-define('DATABASE_NAME', 'ample');
+define('DATABASE_NAME', 'u510162695_ample');
 // Database DB_PASS
-define('DATABASE_PASS','');
+define('DATABASE_PASS','1Ample_database');
 
 
  ?>
