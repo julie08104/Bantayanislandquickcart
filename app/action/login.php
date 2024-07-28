df67292c4febc66f192977d1fff3ea941442c309
Julie Ann Almohallas <almohallasjulieann08@gmail.com>
2024-07-29 00:09:38 +0800

    commit

--- app/action/login.php
+++ app/action/login.php
@@ -2,15 +2,19 @@
 session_start();
 require_once '../init.php';
 
-// Initialize User object with PDO
-$Ouser = new User($pdo);
-
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $username = $_POST['username'];
     $password = $_POST['password'];
 
-    // Attempt to login
-    $Ouser->login($username, $password);
+    $Ouser = new User($pdo);
+    if ($Ouser->login($username, $password)) {
+        $_SESSION['user_id'] = $Ouser->get_user_id();
+        header("Location: ../../index.php");
+        exit();
+    } else {
+        $_SESSION['login_error'] = 'Invalid username or password';
+        header("Location: ../../login.php");
+        exit();
+    }
 }
 ?>
-
