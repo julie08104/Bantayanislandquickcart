c760950048ebfeeb1719a8896493bb51e71b8f28
Julie Ann Almohallas <almohallasjulieann08@gmail.com>
2024-08-02 08:38:38 +0800

    commit

--- pages/buy_list.php
+++ pages/buy_list.php
@@ -1,5 +1,5 @@
 <?php
-require_once '/index.php/init.php'; // Adjust this path
+require_once '/buy_list.php/init.php'; // Adjust this path
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
