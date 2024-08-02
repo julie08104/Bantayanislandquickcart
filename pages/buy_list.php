56c1f3314f67190712ba8cca5f7664bba0ec7fdc
Julie Ann Almohallas <almohallasjulieann08@gmail.com>
2024-08-02 08:45:22 +0800

    commit

--- pages/buy_list.php
+++ pages/buy_list.php
@@ -1,4 +1,7 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
 // Create Rider
 function createRider($name, $lastname, $gender, $address, $contact_number, $email, $vehicle_type, $license_number, $status, $total_rides, $rating, $payment_method) {
