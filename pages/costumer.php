33cf65ccc8e1a787d6ec717d14e5ac88d434cd39
Julie Ann Almohallas <almohallasjulieann08@gmail.com>
2024-08-02 19:28:36 +0800

    commit

--- pages/costumer.php
+++ pages/costumer.php
@@ -92,7 +92,7 @@ $customers = readCustomers();
             <?php
             $counter = 1; // Initialize counter variable
             foreach ($customers as $customer): ?>
-            <div class="class="float-left mb-3"" role="group">
+            <div class="class="float-left mb-3"" role="group" style="float: left;">
             <button class="btn btn-success" data-toggle="modal" data-target="#addCustomerModal">
                                 <i class="fas fa-plus"></i> Add</button> <br> 
                 <tr>
