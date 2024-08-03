faec253eb9909b0e2d66a27e51729b319e23e454
Julie Ann Almohallas <almohallasjulieann08@gmail.com>
2024-08-03 13:33:33 +0800

    commit

--- pages/costumer.php
+++ pages/costumer.php
@@ -65,6 +65,7 @@ $customers = readCustomers();
 <br>
 <div class="container-fluid" style="margin-left: 0px!important;">
     <h1>Customer List</h1>
+    <!-- add customer-->
     <div class="class="float-left mb-3"" role="group" style="float:left;">
     <button class="btn btn-success" data-toggle="modal" data-target="#addCustomerModal"> <i class="fas fa-plus"></i> Add</button> <br> <br> 
     <!-- Print Button -->
