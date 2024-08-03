8e7549fde0485eb70e23af68173f67801246c1b0
julie08104 <almohallasjulieann08@gmail.com>
2024-08-04 02:54:48 +0800

    Update costumer.php

--- pages/costumer.php
+++ pages/costumer.php
@@ -68,7 +68,6 @@ $customers = readCustomers();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Customer List</title>
     <link rel="stylesheet" href="path/to/bootstrap.css">
     <link rel="stylesheet" href="path/to/datatables.css">
     <style>
