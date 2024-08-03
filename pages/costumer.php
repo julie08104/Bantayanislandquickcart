0f55a95b0266d29dd9f6a15f9f4e012c40867b5a
julie08104 <almohallasjulieann08@gmail.com>
2024-08-04 03:48:52 +0800

    Update costumer.php

--- pages/costumer.php
+++ pages/costumer.php
@@ -105,7 +105,7 @@ $customers = readCustomers();
 <body>
  <!-- Print Image -->
 <div id="printImage" class="print-only">
-    <img src="dist/img/images1.png" alt="logo" class="brand-image" style="display: block; margin: 2px auto; width: 60px; height: auto;">
+    <img src="dist/img/images1.png" alt="logo" class="brand-image" style="display: block; margin: 2px auto; width: 100px; height: auto;">
 </div><br>
   <div class="container-fluid" style="margin-left: 0px!important;">
         <h1>Customer List</h1>
