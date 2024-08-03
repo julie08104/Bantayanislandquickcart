92efa895f8c9b6fb9725b59d9facf2097954ac7e
julie08104 <almohallasjulieann08@gmail.com>
2024-08-04 02:04:00 +0800

    Update costumer.php

--- pages/costumer.php
+++ pages/costumer.php
@@ -91,7 +91,7 @@ $customers = readCustomers();
 <body>
     <!-- Print Image -->
     <div id="printImage" class="print-only">
-        <img src="upload/images.png" alt="Print Image" style="width: 100px; height: auto;">
+        <img src="uploads/images.png" alt="Print Image" style="width: 100px; height: auto;">
     </div>
 
     <div class="container-fluid" style="margin-left: 0px!important;">
