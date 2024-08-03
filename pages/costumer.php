15cd2eb0fc453f7255ef714b3275295cf9a2e47a
julie08104 <almohallasjulieann08@gmail.com>
2024-08-04 01:19:06 +0800

    Update costumer.php

--- pages/costumer.php
+++ pages/costumer.php
@@ -69,7 +69,7 @@ $customers = readCustomers();
 <div class="container-fluid" style="margin-left: 0px!important;">
     <h1>Customer List</h1>
 <!--add customer-->
-<div class="float-left mb-3" role="group" style="float:left;">
+<div class="float-left mb-3"  style="float:left;">
             <button class="btn btn-success" data-toggle="modal" data-target="#addCustomerModal">
                                 <i class="fas fa-plus"></i> Add Customer </button> <br> <br> 
     <!-- Print Button -->
