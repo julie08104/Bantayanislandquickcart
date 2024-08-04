2108f0911756c1ca3ff1c0fe35771041fe0e1dce
julie08104 <almohallasjulieann08@gmail.com>
2024-08-04 11:18:47 +0800

    Update delete_customer.php

--- delete_customer.php
+++ delete_customer.php
@@ -1,5 +1,9 @@
 <?php
 require '../init.php';
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 header('Content-Type: application/json');
 include 'database_connection.php'; // Adjust path as necessary
 
