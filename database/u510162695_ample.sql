d634bd4bf2dee65ebc72d0eb0b2476d91f67bfe3
julie08104 <almohallasjulieann08@gmail.com>
2024-08-04 12:44:43 +0800

    Update u510162695_ample.sql

--- database/u510162695_ample.sql
+++ database/u510162695_ample.sql
@@ -57,7 +57,7 @@ CREATE TABLE `category` (
 -- Table structure for table `customer`
 --
 
-CREATE TABLE customers (
+CREATE TABLE customer (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     lastname VARCHAR(255) NOT NULL,
