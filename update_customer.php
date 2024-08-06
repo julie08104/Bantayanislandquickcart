e25b73bdf75035a8821f162791be7c9a5b9cf6b2
julie08104 <almohallasjulieann08@gmail.com>
2024-08-06 14:21:37 +0800

    Update update_customer.php

--- update_customer.php
+++ update_customer.php
@@ -4,17 +4,18 @@ $response = ['success' => false, 'message' => 'An error occurred'];
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // Ensure you receive all the required data
-    if (isset($_POST['id'], $_POST['name'], $_POST['lastname'], $_POST['address'], $_POST['contact'], $_POST['email'])) {
+    if (isset($_POST['id'], $_POST['name'], $_POST['lastname'], $_POST['company'], $_POST['address'], $_POST['contact'], $_POST['email'])) {
         $id = $_POST['id'];
         $name = $_POST['name'];
         $lastname = $_POST['lastname'];
+        $company = $_POST['company'];
         $address = $_POST['address'];
         $contact = $_POST['contact'];
         $email = $_POST['email'];
         
         // Prepare and execute your update statement
-        $stmt = $pdo->prepare("UPDATE customers SET name = ?, lastname = ?, address = ?, contact = ?, email = ? WHERE id = ?");
-        if ($stmt->execute([$name, $lastname, $address, $contact, $email, $id])) {
+        $stmt = $pdo->prepare("UPDATE customers SET name = ?, lastname = ?, company = ?, address = ?, contact = ?, email = ? WHERE id = ?");
+        if ($stmt->execute([$name, $lastname, $company, $address, $contact, $email, $id])) {
             $response['success'] = true;
             $response['message'] = 'Customer updated successfully';
         } else {
