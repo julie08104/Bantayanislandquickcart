9fb6e721d7de543391bf318dab5990305e7124c6
julie08104 <almohallasjulieann08@gmail.com>
2024-08-06 16:20:46 +0800

    Update buy_list.php

--- pages/buy_list.php
+++ pages/buy_list.php
@@ -368,9 +368,6 @@ $riders = readRiders();
 </div>
 
 
-<!-- JavaScript to handle form submissions and modal opening -->
-<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.2/dist/js/bootstrap.bundle.min.js"></script>
 
 <script>
 
