1d769962ae6aa757c159d5402423e1a5fc156dbe
julie08104 <almohallasjulieann08@gmail.com>
2024-08-06 12:19:18 +0800

    Update buy_list.php

--- pages/buy_list.php
+++ pages/buy_list.php
@@ -454,6 +454,7 @@ function openEditModal(rider) {
         document.getElementById('viewVehicleType').value = rider.vehicle_type;
         document.getElementById('viewLicenseNumber').value = rider.license_number;
         document.getElementById('viewStatus').value = rider.status;
+        // Ensure these fields are not present in the modal
         document.getElementById('viewTotalRides').value = rider.total_rides;
         document.getElementById('viewRating').value = rider.rating;
         document.getElementById('viewPaymentMethod').value = rider.payment_method;
