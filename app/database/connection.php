5467035602ea565a45cfd647897ae97929dec475
Julie Ann Almohallas <almohallasjulieann08@gmail.com>
2024-09-10 11:54:00 +0800

    COMMIT

--- app/database/connection.php
+++ app/database/connection.php
@@ -1,11 +1,11 @@
 <?php
-$servername = "127.0.0.1";
-$username = "u510162695_ample";
-$password = "1Ample_database";
-$dbname = "u510162695_ample";
+$servername = "";
+$username = "";
+$password = "";
+$dbname = "";
 
 try {
-    $pdo = new PDO("mysql:host=127.0.0.1;dbname=u510162695_ample", 'u510162695_ample','1Ample_database');
+    $pdo = new PDO("mysql:host=;dbname=", '','');
     // Set the PDO error mode to exception
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     // Set charset
