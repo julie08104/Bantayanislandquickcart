4bb751d5f0cbc9dedda0407dd12f73d45af6c5bf
julie08104 <almohallasjulieann08@gmail.com>
2024-09-29 10:43:54 +0800

    apply sweetalert

--- alert.php
+++ alert.php
@@ -1,8 +1,18 @@
 <?php
     if (isset($_SESSION['message'])) {
         $message = $_SESSION['message'];
-        $class = $message['type'] == 'success' ? "p-4 mb-4 text-sm text-green-800 rounded-lg bg-green-50" : 'p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-50';
-        echo "<div class='message $class'>{$message['text']}</div>";
+        $type = $message['type'] == 'success' ? 'success' : 'error';
+        $text = $message['text'];
+
+        echo "<script>
+            Swal.fire({
+                icon: '$type',
+                title: '" . ($type == 'success' ? 'Success!' : 'Error!') . "',
+                text: '$text',
+                confirmButtonText: 'OK'
+            });
+        </script>";
+
         unset($_SESSION['message']);
     }
 ?>
