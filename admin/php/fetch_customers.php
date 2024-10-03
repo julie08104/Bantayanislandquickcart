98b7d6d6e31e4c6101709c825b1218d8d82062a0
julie08104 <almohallasjulieann08@gmail.com>
2024-10-03 21:56:22 +0800

    fix customer raider

--- admin/php/fetch_customers.php
+++ admin/php/fetch_customers.php
@@ -2,7 +2,7 @@
 require '../../config.php';
 
 // Fetch user data
-$stmt = $pdo->query("SELECT id, CONCAT(firstname, ' ', lastname) AS fullname, email, phone, address, is_verified FROM customers");
+$stmt = $pdo->query("SELECT id, CONCAT(firstname, ' ', lastname) AS fullname, email, phone, address, is_verified FROM customers WHERE is_verified = 1");
 $customers = $stmt->fetchAll();
 
 // Output as JSON
--- admin/php/fetch_raiders.php
+++ admin/php/fetch_raiders.php
@@ -2,7 +2,7 @@
 require '../../config.php';
 
 // Fetch user data
-$stmt = $pdo->query("SELECT id, CONCAT(firstname, ' ', lastname) AS fullname, email, phone, address, vehicle_type, vehicle_number, is_verified FROM raiders");
+$stmt = $pdo->query("SELECT id, CONCAT(firstname, ' ', lastname) AS fullname, email, phone, address, vehicle_type, vehicle_number, is_verified FROM raiders WHERE is_verified = 1");
 $raiders = $stmt->fetchAll();
 
 // Output as JSON
