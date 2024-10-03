37340a3642200a1cb99c16880318e924d4b4dbce
julie08104 <almohallasjulieann08@gmail.com>
2024-10-03 10:12:39 +0800

    update db and admin register page

--- db/u510162695_ample.sql
+++ db/u510162695_ample.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 29, 2024 at 03:55 PM
+-- Generation Time: Oct 03, 2024 at 04:11 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -34,13 +34,6 @@ CREATE TABLE `assignments` (
   `assigned_at` datetime DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `assignments`
---
-
-INSERT INTO `assignments` (`id`, `order_id`, `raider_id`, `assigned_at`) VALUES
-(14, 5, 7, '2024-09-29 21:00:30');
-
 -- --------------------------------------------------------
 
 --
@@ -61,13 +54,6 @@ CREATE TABLE `customers` (
   `created_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `customers`
---
-
-INSERT INTO `customers` (`id`, `firstname`, `lastname`, `phone`, `address`, `email`, `password_hash`, `is_verified`, `verification_code`, `forgot_password_code`, `created_at`) VALUES
-(6, 'john', 'doe', '09123456789', 'Madridejos Cebu', 'john@gmail.com', '$2y$10$szaXNhwOi2lCH5Ki42CF9..1O7CAMobETuWT.Mv1fSl/r.Krvyb3S', 1, '', '', '2024-09-29 02:27:16');
-
 -- --------------------------------------------------------
 
 --
@@ -84,13 +70,6 @@ CREATE TABLE `orders` (
   `created_at` datetime DEFAULT current_timestamp(),
   `updated_at` datetime DEFAULT NULL ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `orders`
---
-
-INSERT INTO `orders` (`id`, `customer_id`, `status`, `instruction`, `total_amount`, `delivery_fee`, `created_at`, `updated_at`) VALUES
-(5, 6, 'completed', '1 albertos pizza', 150.00, 15.00, '2024-09-29 20:58:08', '2024-09-29 21:42:33');
 
 -- --------------------------------------------------------
 
@@ -114,13 +93,6 @@ CREATE TABLE `raiders` (
   `created_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `raiders`
---
-
-INSERT INTO `raiders` (`id`, `firstname`, `lastname`, `phone`, `address`, `vehicle_type`, `vehicle_number`, `email`, `password_hash`, `is_verified`, `verification_code`, `forgot_password_code`, `created_at`) VALUES
-(7, 'John', 'Doe', '09123456789', 'Madridejos Cebu', 'Motor', 'Motor1', 'john@gmail.com', '$2y$10$AdRMdavxrCUrKx1CpRQ8ZeA5QoP9ISaYnWK34UyP9RP1A3MxOblVC', 1, '', '', '2024-09-29 02:06:24');
-
 -- --------------------------------------------------------
 
 --
@@ -135,13 +107,6 @@ CREATE TABLE `reviews` (
   `comment` text DEFAULT NULL,
   `created_at` datetime DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `reviews`
---
-
-INSERT INTO `reviews` (`id`, `order_id`, `customer_id`, `rating`, `comment`, `created_at`) VALUES
-(5, 5, 6, 5, 'Deliver on time', '2024-09-29 21:55:20');
 
 -- --------------------------------------------------------
 
@@ -165,7 +130,7 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `email`, `password_hash`, `is_verified`, `verification_code`, `forgot_password_code`, `created_at`) VALUES
-(12, 'Admin', 'admin@gmail.com', '$2y$10$XnVvjRZF/hamNi56Hg/ZC.DzMbTVyW2hJDHCCctO3LnjBIZ6Fctma', 1, NULL, NULL, '2024-09-10 12:32:57');
+(1, 'Admin', 'admin@bantayanquickcart.com', '$2y$10$H7C6RIv0Li0WRXsbHueBuuERk8EwBfwHGmjITs.kBpCML7870cvCa', 0, '5cdb36cd15d94f60a0f6d30674800981', NULL, '2024-10-03 02:10:00');
 
 --
 -- Indexes for dumped tables
@@ -223,7 +188,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `assignments`
 --
 ALTER TABLE `assignments`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `customers`
@@ -235,7 +200,7 @@ ALTER TABLE `customers`
 -- AUTO_INCREMENT for table `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `raiders`
@@ -247,13 +212,13 @@ ALTER TABLE `raiders`
 -- AUTO_INCREMENT for table `reviews`
 --
 ALTER TABLE `reviews`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- Constraints for dumped tables
