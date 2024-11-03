f461765d24930ed37c61d265aec093fafc080163
julie08104 <almohallasjulieann08@gmail.com>
2024-11-03 22:09:38 +0800

    fix leaflet routing

--- header.php
+++ header.php
@@ -12,10 +12,11 @@
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.all.min.js"></script>
     <link rel="stylesheet" href="https://unpkg.com/leaflet@1.2.0/dist/leaflet.css" />
-    <link rel="stylesheet" href="/leaflet-routing-machine/dist/leaflet-routing-machine.css" />
+    <link rel="stylesheet" href="https://unpkg.com/leaflet-routing-machine@latest/dist/leaflet-routing-machine.css" />
     <script src="https://unpkg.com/leaflet@1.2.0/dist/leaflet.js"></script>
-    <script src="/leaflet-routing-machine/dist/leaflet-routing-machine.js"></script>
-    <script src="/leaflet-routing-machine/dist/lrm-graphhopper-1.2.0.min.js"></script>
+    <script src="https://unpkg.com/leaflet-routing-machine@latest/dist/leaflet-routing-machine.js"></script>
+    <!-- <script src="https://unpkg.com/leaflet-routing-machine@latest/dist/lrm-graphhopper-1.2.0.min.js"></script> -->
+    <script src="https://cdn.jsdelivr.net/npm/lrm-graphhopper@1.3.0/src/L.Routing.GraphHopper.min.js"></script>
     <script src="https://cdn.tailwindcss.com"></script>
     <style>
         @media print {
