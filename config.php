c2ecd2f81438c64ed5df41d71a3d8a8f9f9e0a5b
julie08104 <almohallasjulieann08@gmail.com>
2024-12-10 13:49:58 +0800

    fix headers

--- config.php
+++ config.php
@@ -9,6 +9,12 @@ $password = '1Ample_database';
 // $username = 'root';
 // $password = '';
 
+header("Strict-Transport-Security: max-age=31536000; includeSubDomains; preload");
+header("X-Frame-Options: DENY");
+header("X-Content-Type-Options: nosniff");
+header("Referrer-Policy: no-referrer");
+header("Permissions-Policy: geolocation=(), microphone=(), camera=()");
+
 try {
     $pdo = new PDO($dsn, $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
