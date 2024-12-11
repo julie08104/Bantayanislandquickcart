a8b2b80e3206703ed730b36798330ff5f4647ae8
julie08104 <almohallasjulieann08@gmail.com>
2024-12-11 21:47:26 +0800

    fix

--- auth_check.php
+++ auth_check.php
@@ -4,7 +4,7 @@ if (!isset($_SESSION['user_id'])) {
     header("Location: login.php");
     exit();
 }else if($_SESSION['user_type'] !== $page_type) {
-    header("Location: /mcc/".$_SESSION['user_type']."/index.php");
+    header("Location: /".$_SESSION['user_type']."/index.php");
     exit();
 }
 ?>
