bb2c7a942b3d8d9bcea0c43d1369ea41b469f003
julie08104 <almohallasjulieann08@gmail.com>
2024-12-11 22:14:54 +0800

    add terms and conditions page

---
+++ terms_and_conditions.php
@@ -0,0 +1,38 @@
+<?php include 'header.php'; ?>
+
+<div class="w-full max-w-2xl mx-auto py-6">
+    <div class="bg-white shadow rounded p-4 space-y-4">
+        <h1 class="text-2xl text-center">Terms and Conditions</h1>
+        <p>
+            By using Bantayan Island QuickCart, you confirm that you legally agree to these Terms and Conditions. If you do not meet this requirement, please do not use the platform.
+        </p>
+        <ul class="list-disc list-inside">
+            <li>
+                Notify us immediately if you notice unauthorized access to your account.
+            </li>
+            <li>
+                This platforms connects you with local stores for delivery, but we can't guarantee the quality, availability or accuracy of the products they sell.
+            </li>
+            <li>
+                Prices include fees and taxes but may change.
+            </li>
+            <li>
+                Delivery fees depend on your location and service type.
+            </li>
+            <li>
+                Once delivered, items are your responsibility.
+            </li>
+            <li>
+                Do not use the platform for illegal activities.
+            </li>
+            <li>
+                Do not provide false information
+            </li>
+            <li>
+                We are not responsible for any damages or loses after delivery.
+            </li>
+        </ul>
+    </div>
+</div>
+
+<?php include 'footer.php'; ?>
